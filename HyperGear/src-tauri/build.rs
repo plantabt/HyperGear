2311d3ad29ee5eb4c4b53d38a12ba2c80322409d
kahn <keechoo@cyberfear.com>
2024-03-28 13:00:57 +0800

    prepare gear.dll before build

--- HyperGear/src-tauri/build.rs
+++ HyperGear/src-tauri/build.rs
@@ -1,3 +1,26 @@
+use std::{ fs, path::Path};
+
 fn main() {
+    let red = "\x1b[31m";
+   // let yellow = "\x1b[33m";
+    let reset = "\x1b[0m"; 
+
+    let mut source = Path::new("../../target/release/gear.dll");
+    let mut errinfo ="error: Please run [ cargo build -p gear --release ] first!!!";
+    #[cfg(debug_assertions)]{
+        source = Path::new("../../target/debug/gear.dll");
+        errinfo="error: Please run [ cargo build -p gear ] first!!!";
+    }
+    let destination = Path::new("./gear.dll");
+    println!("copy gear.dll -->{:?} - {:?}",source,destination);
+    match fs::copy(&source, &destination) {
+        Ok(_) => println!("File copied successfully to {:?}", destination),
+
+        Err(_) => {
+            print!("{}", red);
+            print!("{}", errinfo);
+            println!("{}", reset);
+        },
+    }
     tauri_build::build()
 }
