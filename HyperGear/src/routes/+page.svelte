e9c7827e4eef2888b904ad06194cab92d7568c4d
kahn <keechoo@cyberfear.com>
2024-04-03 15:07:18 +0800

    add process search list

--- HyperGear/src/routes/+page.svelte
+++ HyperGear/src/routes/+page.svelte
@@ -3,17 +3,12 @@
 	//import { MessageBox } from '$lib/utilis';
 	import {CDelegate} from '$lib/delegate'
 	import {globalVar} from '$lib/global'
-	
-	import { faker } from '@faker-js/faker';
-	import { ListBox, ListBoxItem, Table, type PaginationSettings, type TableSource, tableMapperValues,  ProgressRadial, RangeSlider, SlideToggle, type ModalSettings, initializeStores, getModalStore, ProgressBar} from '@skeletonlabs/skeleton';
-	import { Paginator } from '@skeletonlabs/skeleton';
-    import { invoke } from '@tauri-apps/api';
-    import { appWindow } from '@tauri-apps/api/window';
+	// Types
+	import type { AutocompleteOption, PopupSettings } from '@skeletonlabs/skeleton';
+	import { ProgressRadial, RangeSlider, SlideToggle,Autocomplete, popup} from '@skeletonlabs/skeleton';
 	import Icon from 'svelte-awesome';
 	import   {userSecret}from 'svelte-awesome/icons';
-    import { emit } from '@tauri-apps/api/event';
-    import { getVersion } from '@tauri-apps/api/app';
-	import { registerAll, unregister, unregisterAll } from '@tauri-apps/api/globalShortcut';
+
 
     let g_version:string="";
 	let toggle_speed=true;
@@ -21,9 +16,37 @@
 	let max:number =100;
 	let g_exe_name="";
 	let speadtxt=0;
+
+	let popupSettings: PopupSettings = {
+		event: 'click',
+		target: 'popupAutocomplete',
+		placement: 'bottom',
+	};
+	let g_process_selected: string = '';
 	
+	type FlavorOption = AutocompleteOption<string, { healthy: boolean }>;
+	let flavorOptions: FlavorOption[] = [];
 
 
+	function onPopupDemoSelect(event: CustomEvent<FlavorOption>): void {
+		g_process_selected = event.detail.label;
+	}
+	async function update_process_list(){
+		let process_list = await CDelegate.GetProcessList();
+		flavorOptions=null;
+		flavorOptions = [];
+		for(let i in process_list){
+			flavorOptions.push({label: process_list[i]["name"] + " - [ " + process_list[i]["pid"] + " ]", value: process_list[i]["pid"], keywords: 'processlist', meta: { healthy: false }});
+		}
+	}
+	function onInputProcname(e:Event){
+
+		update_process_list();
+	}
+
+	function updatelist(e:Event){
+		update_process_list();
+	}
 	async function init(){
 
 		g_version = await CDelegate.GetVersion();
@@ -33,7 +56,16 @@
 
 
 	async function injection(e:Event){
+		let split_pname = g_process_selected.split("-");
+		if(split_pname.length<2){
+			CDelegate.MessageBox("Inject error","Process name error,format is:  procname - [ pid ] ");
+			return;
+		}
+		g_exe_name = split_pname[0].trim();
+		let pid = parseInt(split_pname[1].match(/[0-9]{1,10}/)[0]);
+		console.log(pid);
 		if(g_exe_name==""){
+			CDelegate.MessageBox("Inject error","Input a process first!");
 			return;
 		}
 
@@ -44,7 +76,7 @@
 		await CDelegate.WriteSpeed(g_exe_name,$globalVar.exe_info[0].speed);
 
 		let dllpath = await CDelegate.GetCurrentDir()+"\\gear.dll";
-		if(await CDelegate.Inject(g_exe_name,dllpath)!=0){
+		if(await CDelegate.InjectByPid(pid,dllpath)!=0){
 			CDelegate.MessageBox("Good Job.","Speedhacking injected!");
 		}
 
@@ -93,27 +125,67 @@ $: {
 </script>
 
 <!-- svelte-ignore a11y-no-static-element-interactions -->
-<div data-tauri-drag-region class="container h-full mx-auto flex justify-center items-center"  >
+<div  class="container h-full mx-auto flex justify-center items-center"  >
 	
 	<!-- svelte-ignore a11y-click-events-have-key-events -->
-	<div data-tauri-drag-region class="space-y-6 text-center flex flex-col items-center " >
+	<div class="space-y-6 text-center flex flex-col items-center " >
 	
-		<h2 data-tauri-drag-region class="h2 mt-[-20px] mb-[60px]" >{g_version}</h2>
-		
-		<div class="input-group grid-cols-[auto_1fr_auto] h-[32px] text-[12px]" >
-			<div class="input-group-shim h-[30px] text-[12px]" >PROCESS</div>
-			<input type="search" class="h-[30px] text-[12px]" placeholder="Input name of exe..." bind:value={g_exe_name}/>
-			<button class="variant-filled h-[30px] text-[12px]" on:click={injection}><Icon data={userSecret}/> (INJECT)</button>
+		<h2 data-tauri-drag-region class="h2 h-10 -m-24 mb-2 w-[800px] pt-8 pb-14" >{g_version}</h2>
+		<!--
+		<input class="input autocomplete" type="search" name="autocomplete-search" bind:value={g_process_selected} placeholder="Search..." use:popup={popupSettings} />
+		<div data-popup="popupAutocomplete">
+			<Autocomplete bind:input={g_process_selected} options={flavorOptions} on:selection={onPopupDemoSelect}		/>
+		</div>-->
+		<div class="flex">
+			<div class="flex items-center justify-center bg-gray-500  h-7 w-16 text-xs  rounded-l-3xl" >PROCESS</div>
+			<div class="flex-1 text-token space-y-1 z-50 h-7 ">
+				<input
+					class="input text-xs rounded-none h-7 border border-gray-600"
+					type="search"
+					name="autocomplete-search"
+					bind:value={g_process_selected}
+					placeholder="Search Process..."
+					use:popup={popupSettings}
+					on:click={updatelist}
+					on:input={onInputProcname}
+					autocomplete="off"
+				/>
+				<div data-popup="popupAutocomplete" class="card max-w-xs max-h-32 p-2 overflow-y-auto text-xs rounded-md " tabindex="-1">
+					<Autocomplete bind:input={g_process_selected} options={flavorOptions} on:selection={onPopupDemoSelect}/>
+				</div>
+			</div>
+			<button class="flex items-center justify-center variant-filled h-7 w-20 text-xs rounded-r-3xl" on:click={injection}><Icon data={userSecret}/> (INJECT)</button>
 		</div>
+		
+		<!--<input type="search" class="h-[30px] text-[12px]" placeholder="Input name of exe..." bind:value={g_exe_name}/>-->
+		<!--<div class="input-group grid-cols-[auto_1fr_auto] h-8 text-xs" >
+			<div class="input-group-shim h-[30px] text-xs" >PROCESS</div>
+			
+			<div class="text-token space-y-1 z-50">
+				<input
+					class="autocomplete text-xs"
+					type="search"
+					name="autocomplete-search"
+					bind:value={g_process_selected}
+					placeholder="Search..."
+					use:popup={popupSettings}
+				/>
+				<div data-popup="popupAutocomplete" class="card max-w-xs max-h-32 p-2 overflow-y-auto text-xs" tabindex="-1">
+					<Autocomplete bind:input={g_process_selected} options={flavorOptions} on:selection={onPopupDemoSelect} />
+				</div>
+			</div>
+			<button class="variant-filled h-[30px] text-xs" on:click={injection}><Icon data={userSecret}/> (INJECT)</button>
+			</div>
+		</div>-->
 		<br/>
-		<ProgressRadial value={g_speed} class="h-[120px] w-[120px]">{(g_speed/10).toFixed(2)}</ProgressRadial>
+		<ProgressRadial value={g_speed} class=" h-[110px] w-[110px]">{(g_speed/10).toFixed(2)}</ProgressRadial>
 		<!--<ProgressBar label="Progress Bar" value={value} max={max} />-->
 		<div class="space-y-1 text-center flex flex-col items-center " >
 		<div class="flex justify-between items-center">
-			<input class="input h-[24px] text-[12px] w-[120px] mr-[20px]" type="text" bind:value={speadtxt} />
-			<button type="button" class="btn variant-filled h-[24px] w-[50px] text-[12px]" on:click={apply}>APPLY</button>
+			<input class="input h-6 text-xs w-32 mr-5" type="text" bind:value={speadtxt} />
+			<button type="button" class="btn variant-filled h-6 w-12 text-xs" on:click={apply}>APPLY</button>
 		</div>
-		<RangeSlider name="range-slider" bind:value={g_speed} max={max} step={0.001} class="w-[198px]" >
+		<RangeSlider name="range-slider" bind:value={g_speed} max={max} step={0.001} class=" w-48" >
 			<!--<div class="flex justify-between items-center">
 				<div class="font-bold">[0 to 10] times:</div>
 				<div class="text-xs">{(g_speed/10).toFixed(2)}</div>
